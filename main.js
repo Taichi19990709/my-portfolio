f3e4dabb512cec6d7f475bf7d8edc29bba8476c1
watanabe <iamogroman@gmail.com>
2022-02-05 17:34:43 +0900

    ポートフォリオサイトの準備をしました

---
+++ main.js
@@ -0,0 +1,29 @@
+const university = document.getElementById("university");
+const job = document.getElementById("job");
+const basketball = document.getElementById("basketball");
+const hiphop = document.getElementById("hiphop");
+const main = document.getElementById("main");
+const press = document.getElementById("press");
+const aho = document.getElementById("aho");
+university.onclick = function () {
+  main.textContent = "大阪大学人間科学部";
+};
+
+job.onclick = function () {
+  main.textContent = "天下一品";
+};
+
+basketball.onclick = function () {
+  main.textContent = "バスケサークル";
+};
+
+hiphop.onclick = function () {
+  main.textContent = "Hiphop";
+};
+
+press.onclick = function () {
+  const apaa = document.createElement("div");
+  aho.prepend(apaa);
+  apaa.className = "apaa";
+  apaa.textContent = "まぁ何も起こらんけど。";
+};
