3fe790852b91a9dd96f7f9995a06ece4be1e3984
watanabe <iamogroman@gmail.com>
2022-02-05 18:16:44 +0900

    a

--- main.js
+++ main.js
@@ -5,10 +5,14 @@ const hiphop = document.getElementById("hiphop");
 const main = document.getElementById("main");
 const press = document.getElementById("press");
 const aho = document.getElementById("aho");
+const home = document.getElementById("home");
 university.onclick = function () {
   main.textContent = "大阪大学人間科学部";
 };
 
+home.onclick = function () {
+  main.textContent = "";
+};
 job.onclick = function () {
   main.textContent = "天下一品";
 };
